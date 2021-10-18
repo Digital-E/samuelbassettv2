bb1998868fe1e9d7e55869459ded4fdc00b3aed5
Samuel Bassett <samabassett@gmail.com>
2021-10-18 22:53:08 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withImages = require('next-images')
 module.exports = withImages()
 
-// module.exports = {
-//     images: {
-//       disableStaticImages: true
-//     }
-//   }
+module.exports = {
+    webpack5: false
+    // images: {
+    //   disableStaticImages: true
+    // }
+  }
