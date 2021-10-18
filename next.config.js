71c1e11c98ed058d478e483230c13e54312e916d
Samuel Bassett <samabassett@gmail.com>
2021-10-18 21:57:48 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
-const withImages = require('next-images')
-module.exports = withImages()
+// const withImages = require('next-images')
+// module.exports = withImages()
+
+module.exports = {
+    images: {
+      disableStaticImages: true
+    }
+  }
